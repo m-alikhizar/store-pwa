df06f3c8b6e65bdc48f925df24a399d26da3badd
Khizar <khizar.ali.manzoor@gmail.com>
2018-06-30 07:55:50 +0500

    Write configurations in webpack.config.js to bundle app assets.

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const combineLoaders = require('webpack-combine-loaders');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname + '/dist'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+
+            query: {
+              modules: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]'
+            }
+          }
+        ]
+      },
+
+      {
+        test: /\.css$/,
+        include: /node_modules/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+};
