43dc22f316c29672ca396bd9cf4b8a273e8918ab
Khizar <khizar.ali.manzoor@gmail.com>
2018-06-30 08:33:30 +0500

    Added action type constants

---
+++ src/constants/ActionTypes.js
@@ -0,0 +1,10 @@
+'use statics';
+
+export default {
+  TOGGLE_PROPERTY: 'toggle_property',
+
+  FETCH_PRODUCTS_REQUEST: 'fetch_products_request',
+  FETCH_PRODUCTS_RECEIVE: 'fetch_products_receive',
+  FETCH_PRODUCTS_ERROR: 'fetch_products_error'
+
+}
