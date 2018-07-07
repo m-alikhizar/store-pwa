1bdf2fe97dc0d4e4e17cdebe3aaffa793df61fd6
Khizar <khizar.ali.manzoor@gmail.com>
2018-07-07 17:10:31 +0500

    Send components as children prop to item component.

--- src/containers/ProductDetails.js
+++ src/containers/ProductDetails.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { addItem, addToCart } from '../actions';
+import Item from '../components/Item';
 import { Container, Card, CardImg, CardBody, CardTitle, CardText, Button } from 'reactstrap';
 
-class Item extends React.Component {
+class ItemDetails extends React.Component {
   constructor(props) {
     super(props);
   }
@@ -19,23 +20,18 @@ class Item extends React.Component {
 
     return (
       <Container style={{margin: '90px 0'}} fluid={true}>
-        <Card style={{ flexDirection: 'row'}}>
-          <CardImg top style={{width: 'auto', height: '400px'}} src={item.img} alt="Card image cap" />
-          <CardBody>
-            <CardTitle>{item.name}</CardTitle>
-            <CardText>{item.price}</CardText>
-            <Button size="lg" onClick={() => dispatch(addToCart({id: item.id, quantity: 1}))} color="primary">Add to Cart</Button>
-          </CardBody>
-        </Card>
+        <Item {...item} children={action}/>
       </Container>
     );
   }
 }
 
+const action = (<Button size="lg" onClick={() => dispatch(addToCart({id: item.id, quantity: 1}))} color="primary">Add to Cart</Button>);
+
 const mapStateToProps = (store) => ({ item: store.item });
 
 const mapDispatchToProps = (dispatch) => ({ dispatch })
 
-const ProductDetails = connect(mapStateToProps, mapDispatchToProps)(Item);
+const ProductDetails = connect(mapStateToProps, mapDispatchToProps)(ItemDetails);
 
 export default ProductDetails;
