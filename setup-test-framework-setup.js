32d3d7cbd2600a62db49e1b79c88343d8e146384
Khizar <khizar.ali.manzoor@gmail.com>
2018-07-10 01:41:28 +0500

    Combined chai & jest assertion libraries w.r.t expect in single dependency.

---
+++ setup-test-framework-setup.js
@@ -0,0 +1,28 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import chai from 'chai';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+// Make sure chai and jasmine ".not" play nice together
+const originalNot = Object.getOwnPropertyDescriptor(chai.Assertion.prototype, 'not').get;
+Object.defineProperty(chai.Assertion.prototype, 'not', {
+  get() {
+    Object.assign(this, this.assignedNot);
+    return originalNot.apply(this);
+  },
+  set(newNot) {
+    this.assignedNot = newNot;
+    return newNot;
+  },
+});
+
+// Combine both jest and chai matchers on expect
+const originalExpect = global.expect;
+
+global.expect = (actual) => {
+  const originalMatchers = originalExpect(actual);
+  const chaiMatchers = chai.expect(actual);
+  const combinedMatchers = Object.assign(chaiMatchers, originalMatchers);
+  return combinedMatchers;
+};
