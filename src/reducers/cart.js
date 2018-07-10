958ab78f556bc7508e35dc610c25162d18987532
Khizar <khizar.ali.manzoor@gmail.com>
2018-07-10 20:20:04 +0500

    Added static type checking using flow. Added new rules for ESLint and used correct peer dependencies.

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,3 +1,4 @@
+/* @flow */
 import ActionTypes from '../constants/ActionTypes';
 import Storage from '../helpers/storage';
 
@@ -6,13 +7,10 @@ const defaultState = {
   count: 0
 };
 
-const cart = (state = defaultState, action) => {
-
-  switch(action.type) {
-
-    case ActionTypes.ADD_TO_CART:
-
-      const items = state.items;
+const cart = (state = defaultState, action: object) => {
+  switch (action.type) {
+    case ActionTypes.ADD_TO_CART: {
+      const { items } = state;
       const item = {
         ...action.item,
         quantity: action.quantity
@@ -20,39 +18,36 @@ const cart = (state = defaultState, action) => {
 
       const atIdx = items.findIndex(it => it.id === item.id);
 
-      if(atIdx != -1) {
+      if (atIdx !== -1) {
         items[atIdx].quantity += item.quantity;
       } else {
         items.push(item);
       }
 
-      state.price = items.reduce((total, i) => ((i.price * i.quantity) + total), 0);
-      state.count = items.reduce((count, i) => (i.quantity + count), 0);
+      const price = items.reduce((total, i) => i.price * i.quantity + total, 0);
+      const count = items.reduce((total, i) => i.quantity + total, 0);
 
-      const newState = { ...state, items };
+      const newState = {
+        ...state,
+        items,
+        price,
+        count
+      };
 
-      Storage.write('cart', JSON.stringify(newState))
-      .then(() => {
-        console.log('Storage syncing success.');
-      })
-      .catch(() => {
-        console.log('Storage syncing failed.');
-      });
+      Storage.write('cart', JSON.stringify(newState));
 
       return newState;
-
-    case ActionTypes.CHECKOUT:
-
-      Storage.purge('cart', JSON.stringify(newState))
-      .finally(() => {
+    }
+    case ActionTypes.CHECKOUT: {
+      Storage.purge('cart').finally(() => {
+        // eslint-disable-next-line
         location.href = 'https://www.google.com';
       });
-
       return state;
-
+    }
     default:
-      return state
+      return state;
   }
-}
+};
 
 export default cart;
