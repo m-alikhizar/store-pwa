adb6f68284a1221c695227801ec93b5102341214
Khizar <khizar.ali.manzoor@gmail.com>
2018-07-11 00:46:04 +0500

    Added test for component header, snapshot tests included!

---
+++ src/__tests__/containers/Cart.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import configureStore from 'redux-mock-store';
+import Cart from '../../containers/Cart';
+
+fdescribe('<Cart />', () => {
+  let store = {};
+
+  beforeAll(() => {
+    const middlewares = [];
+    const mockStore = configureStore(middlewares);
+    store = mockStore({ cart: { items: [] } });
+  });
+
+  it('should render without crashing', () => {
+    const wrapper = shallow(<Cart />, { context: { store } });
+
+    expect(wrapper).to.be.a('object');
+  });
+
+  it('should have a match snapshot', () => {
+    const wrapper = shallow(<Cart />, { context: { store } });
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
--- src/containers/Cart.js
+++ src/containers/Cart.js
@@ -1,30 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Button, Popover, PopoverHeader, PopoverBody } from 'reactstrap';
+import NumberFormat from 'react-number-format';
+import {
+  Button, Popover, PopoverHeader, PopoverBody
+} from 'reactstrap';
 import Item from '../components/Item';
 import { checkout } from '../actions';
-import NumberFormat from 'react-number-format';
 import styles from '../styles/Cart.css';
 
 class ItemCart extends React.Component {
   constructor(props) {
     super(props);
 
-    this.toggle = this.toggle.bind(this);
     this.state = {
       popoverOpen: false
     };
 
+    this.toggle = this.toggle.bind(this);
     this.onMouseEnter = this.onMouseEnter.bind(this);
     this.onMouseLeave = this.onMouseLeave.bind(this);
-
   }
 
-  toggle() {
-    this.setState({
-      popoverOpen: !this.state.popoverOpen
-    });
-  }
+  componentDidMount() {}
 
   onMouseEnter(e) {
     this.setState({
@@ -38,50 +35,84 @@ class ItemCart extends React.Component {
     });
   }
 
-	render() {
+  toggle() {
+    this.setState({
+      popoverOpen: !this.state.popoverOpen
+    });
+  }
+
+  render() {
     return (
       <div onMouseEnter={this.onMouseEnter}>
-        <div id={'popover-cart'} onClick={this.toggle} onMouseLeave={this.onMouseLeave}>
+        <div id="popover-cart" onClick={this.toggle} onMouseLeave={this.onMouseLeave}>
           <div className={styles.bag}>
-            <i>{this.props.cart.count}</i>
+            <i>
+              {this.props.cart.count}
+            </i>
           </div>
         </div>
-        <Popover className={styles.popover} onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave} placement={'bottom'} isOpen={this.state.popoverOpen} target={'popover-cart'} toggle={this.toggle}>
-          <div className={styles.overlay}></div>
+        <Popover
+          className={styles.popover}
+          onMouseEnter={this.onMouseEnter}
+          onMouseLeave={this.onMouseLeave}
+          placement="bottom"
+          isOpen={this.state.popoverOpen}
+          target="popover-cart"
+          toggle={this.toggle}
+        >
+          <div className={styles.overlay} />
           <PopoverBody className={styles.body}>
-            { this.props.items.map(item => (<Item key={item.id} {...item} children={<ItemActions quantity={item.quantity} />} />))}
-            { !this.props.items.length && ' ITEMS' }
+            {this.props.items
+              && this.props.items.map(item => (
+                <Item key={item.id} {...item} children={<ItemActions quantity={item.quantity} />} />
+              ))}
+            {!this.props.items.length && ' ITEMS'}
           </PopoverBody>
 
-          { this.props.items.length != 0 && <div className={styles.actions}>
-            <span>
-            TOTAL <NumberFormat
-              displayType={'text'}
-              value={this.props.cart.price}
-              thousandSeparator={true}
-              suffix={' AED'} />
-            </span>
-            <Button size="sm" onClick={() => this.props.dispatch(checkout(this.props.items))}>Secure Checkout</Button>
-          </div> }
+          {this.props.items.length != 0 && (
+            <div className={styles.actions}>
+              <span>
+
+                TOTAL
+                {' '}
+                <NumberFormat
+                  displayType="text"
+                  value={this.props.cart.price}
+                  thousandSeparator
+                  suffix=" AED"
+                />
+              </span>
+              <Button size="sm" onClick={() => this.props.dispatch(checkout(this.props.items))}>
+
+                Secure Checkout
+              </Button>
+            </div>
+          )}
         </Popover>
       </div>
     );
   }
 }
 
-const ItemActions = ({ quantity }) => {
-  return (
-    <div> Quantity: {quantity}</div>
-  )
-}
+const ItemActions = ({ quantity }) => (
+  <div>
+    {' '}
 
-const mapStateToProps = (store) => ({
+    Quantity:
+    {quantity}
+  </div>
+);
+
+const mapStateToProps = store => ({
   cart: store.cart,
   items: store.cart.items
 });
 
-const mapDispatchToProps = (dispatch) => ({ dispatch })
+const mapDispatchToProps = dispatch => ({ dispatch });
 
-const Cart = connect(mapStateToProps, mapDispatchToProps)(ItemCart);
+const Cart = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ItemCart);
 
 export default Cart;
