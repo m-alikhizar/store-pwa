15f2e8a6f96698ad6dd8d973fa14188494529380
Khizar <khizar.ali.manzoor@gmail.com>
2018-07-11 08:06:11 +0500

    Added autocomplete container unit/integration tests.

---
+++ src/__tests__/containers/SearchAutocomplete.test.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import sinon from 'sinon';
+import toJson from 'enzyme-to-json';
+import configureStore from 'redux-mock-store';
+import SearchAutocomplete from '../../containers/SearchAutocomplete';
+import ActionTypes from '../../constants/ActionTypes';
+
+describe('<SearchAutocomplete />', () => {
+  let store = {};
+
+  beforeAll(() => {
+    const middleware = () => store => next => (action) => {
+      let promise;
+
+      switch (action.type) {
+        case ActionTypes.APPLY_SEARCH_CRITERIA:
+        case ActionTypes.FETCH_SEARCH_SUGGESTIONS:
+          promise = new Promise((resolve) => {
+            resolve();
+          });
+          break;
+
+        default:
+          break;
+      }
+
+      next(action);
+      return promise;
+    };
+
+    const middlewares = [middleware()];
+    const mockStore = configureStore(middlewares);
+    store = mockStore({
+      search: {
+        suggestions: []
+      }
+    });
+  });
+
+  it('should render without crashing', () => {
+    const wrapper = shallow(<SearchAutocomplete />, { context: { store } });
+
+    expect(wrapper).to.be.a('object');
+  });
+
+  it('component should have been mounted', () => {
+    const spy = sinon.spy(SearchAutocomplete.prototype, 'componentDidMount');
+
+    const wrapper = mount(<SearchAutocomplete />, { context: { store } });
+
+    expect(spy.callCount).to.equal(1);
+  });
+
+  it('should have a match snapshot', () => {
+    const wrapper = shallow(<SearchAutocomplete />, { context: { store } });
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
