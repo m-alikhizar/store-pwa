dc02a25a2a6db1b525fb9cb0107d852118d8894e
Khizar <khizar.ali.manzoor@gmail.com>
2018-07-12 23:38:27 +0500

    Updated action tests.

--- __tests__/actions/actions.test.js
+++ __tests__/actions/actions.test.js
@@ -2,66 +2,81 @@ import * as Actions from '../../src/actions';
 import ActionTypes from '../../src/constants/ActionTypes';
 
 describe('actions', () => {
-  it('should create an action to list All Items', () => {
+  it('should create an action to get Initial Items', () => {
+    const expected = {
+      type: ActionTypes.GET_INITIAL_ITEMS_REQUEST
+    };
+    expect(Actions.getInitialItemsRequest()).to.deep.equal(expected);
+  });
+
+  it('should create an action to get items request', () => {
+    const index = 1;
+    const expected = {
+      type: ActionTypes.GET_ITEMS_REQUEST,
+      index
+    };
+    expect(Actions.getItemsRequest(index)).to.deep.equal(expected);
+  });
+
+  it('should create an action to get items error', () => {
+    const expected = {
+      type: ActionTypes.GET_ITEMS_ERROR
+    };
+    expect(Actions.getItemsError()).to.deep.equal(expected);
+  });
+
+  it('should create an action to get items receive', () => {
     const items = [
       {
         id: 1,
         title: 'title'
       }
     ];
-
     const expected = {
-      type: ActionTypes.ALL_ITEMS,
+      type: ActionTypes.GET_ITEMS_RECEIVE,
       items
     };
-
-    expect(Actions.allItems(items)).to.deep.equal(expected);
+    expect(Actions.getItemsReceive(items)).to.deep.equal(expected);
   });
 
-  it('should create an action to Initial Items', () => {
-    const index = 0;
-
-    const expected = {
-      type: ActionTypes.INITIAL_ITEMS,
-      index
+  it('should create an action to set filters', () => {
+    const filters = {
+      query: 'test',
+      order: 'asc',
+      key: 'price'
     };
-    expect(Actions.initialItems(index)).to.deep.equal(expected);
-  });
-
-  it('should create an action to Add Item', () => {
-    const id = 0;
 
     const expected = {
-      type: ActionTypes.ADD_ITEM,
+      type: ActionTypes.SET_FILTERS,
+      ...filters
+    };
+
+    expect(Actions.setFilters(filters)).to.deep.equal(expected);
+  });
+
+  it('should create an action to get item request', () => {
+    const id = 1;
+
+    const expected = {
+      type: ActionTypes.GET_ITEM_REQUEST,
       id
     };
 
-    expect(Actions.addItem(id)).to.deep.equal(expected);
+    expect(Actions.getItemRequest(id)).to.deep.equal(expected);
   });
 
-  it('should create an action to Apply Products Update', () => {
-    const items = [{ id: 1 }];
+  it('should create an action to get item receive', () => {
+    const item = [{ id: 1, title: 'name' }];
 
     const expected = {
-      type: ActionTypes.APPLY_PRODUCTS_UPDATE,
-      items
+      type: ActionTypes.GET_ITEM_RECEIVE,
+      item
     };
 
-    expect(Actions.applyProductsUpdate(items)).to.deep.equal(expected);
+    expect(Actions.getItemReceive(item)).to.deep.equal(expected);
   });
 
-  it('should create an action to Apply Search Criteria', () => {
-    const query = 'test';
-
-    const expected = {
-      type: ActionTypes.APPLY_SEARCH_CRITERIA,
-      query
-    };
-
-    expect(Actions.applySearchCriteria(query)).to.deep.equal(expected);
-  });
-
-  it('should create an action to Get Search Suggestions', () => {
+  it('should create an action to get search suggestions', () => {
     const query = 'test';
 
     const expected = {
@@ -72,14 +87,31 @@ describe('actions', () => {
     expect(Actions.getSearchSuggestions(query)).to.deep.equal(expected);
   });
 
-  it('should create an action to Add to Cart', () => {
-    const props = { id: 1, item: {} };
+  it('should create an action to Add item to Cart', () => {
+    const item = {
+      id: 1,
+      title: 'title'
+    };
 
     const expected = {
       type: ActionTypes.ADD_TO_CART,
-      ...props
+      item
     };
+    expect(Actions.addToCart(item)).to.deep.equal(expected);
+  });
 
-    expect(Actions.addToCart(props)).to.deep.equal(expected);
+  it('should create an action to checkout', () => {
+    const items = [
+      {
+        id: 1,
+        title: 'title'
+      }
+    ];
+
+    const expected = {
+      type: ActionTypes.CHECKOUT,
+      items
+    };
+    expect(Actions.checkout(items)).to.deep.equal(expected);
   });
 });
