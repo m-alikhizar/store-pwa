bea1305c0eac3a378b475e1cd85a04c725d2745a
Khizar <khizar.ali.manzoor@gmail.com>
2018-07-13 05:53:01 +0500

    Invoked explicit event handler provided in props.

--- src/hoc/with/Paginated.js
+++ src/hoc/with/Paginated.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Alert, Button } from 'reactstrap';
-import { getItemsRequest } from '../../actions';
 
 const Paginated = conditionFn => Component => props => (
   <div>
@@ -9,7 +8,7 @@ const Paginated = conditionFn => Component => props => (
       {conditionFn(props) && (
         <Alert className="text-center" style={{ fontSize: '32px' }} color="light ">
           Click here to load Items... &nbsp;&nbsp;&nbsp;
-          <Button color="link" onClick={() => props.dispatch(getItemsRequest())}>
+          <Button color="link" onClick={() => props.getItemsRequest()}>
             Try Again
           </Button>
         </Alert>
