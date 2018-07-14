3e902e0c739d5bf7790a6e6dee409420efdb2342
Khizar <khizar.ali.manzoor@gmail.com>
2018-07-14 09:49:44 +0500

    Moved product-item tag to one step inside to allow it to be identified across views.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,29 +1,30 @@
 import React from 'react';
-import { Media} from 'reactstrap';
+import { Media } from 'reactstrap';
 import PropTypes from 'prop-types';
 
-const Item = ({ img, name, price, id, children }) => {
-  return (
-    <div style={{padding: '15px'}}>
-      <Media>
-        <Media left top href="#" style={{marginRight: '15px'}}>
-          <Media object src={img} alt="placeholder image cap" style={{width: '128px'}}/>
-        </Media>
-        <Media body>
-          <Media heading><strong>{name}</strong></Media>
-          <div>Price: {price} AED</div>
-          {children}
-        </Media>
+const Item = ({
+  img, name, price, id, children
+}) => (
+  <div className="product-item" style={{ padding: '15px' }}>
+    <Media>
+      <Media left top href="#" style={{ marginRight: '15px' }}>
+        <Media object src={img} alt="placeholder image cap" style={{ width: '128px' }} />
       </Media>
-    </div>
-  )
-}
-
+      <Media body>
+        <Media heading>
+          <strong>{name}</strong>
+        </Media>
+        <div>Price: {price} AED</div>
+        {children}
+      </Media>
+    </Media>
+  </div>
+);
 
 Item.propTypes = {
   img: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
   id: PropTypes.number.isRequired
-}
-export default Item
+};
+export default Item;
