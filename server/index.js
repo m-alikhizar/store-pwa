b142b98874ba8ac7cc59f63ce529f883221c311a
Khizar <khizar.ali.manzoor@gmail.com>
2018-07-22 21:45:44 +0500

    Listen to PORT 8090 and serving files in production mode.

--- server/index.js
+++ server/index.js
@@ -6,8 +6,21 @@ const app = express();
 
 app.use(express.static(join(__dirname, '..', 'dist')));
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
+  // console.log(`request:${JSON.stringify(req)}`);
   res.sendFile(resolve(__dirname, '..', 'dist', 'index.html'));
 });
 
+app.get(/\/product\/[0-9]+/, (req, res) => {
+  // console.log(`request:${JSON.stringify(req)}`);
+  res.sendFile(resolve(__dirname, '..', 'dist', 'index.html'));
+});
+
+app.get(/(?:service-worker\.js)$/, (req, res) => {
+  // console.log(`request:${JSON.stringify(req)}`);
+  res.sendFile(resolve(__dirname, '..', 'dist', 'service-worker.js'));
+});
+
 app.listen(process.env.PORT || 8090);
+
+console.log('App is listening.');
