75b755e38ce6442f60b8c0843739af098da9b7b2
Khizar <khizar.ali.manzoor@gmail.com>
2018-08-07 07:48:24 +0500

    Used single colon in about page

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import { Container } from 'reactstrap';
 
 const About = () => (
   <Container fluid style={{ margin: '100px' }}>
-    <p className="lead">Github:: https://github.com/khizar-ali/store</p>
+    <p className="lead">Github: https://github.com/khizar-ali/store</p>
   </Container>
 );
 
